16c9d1b9fa1e3236f4e281e11b308b62ff20acad
Lapierre JeanRémi <jeanremi.lapierre@gmail.com>
2024-03-01 14:17:50 +0100

    successfull initialisation of gradle

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("application")
+    id("java")
+}
+
+group = ""
+version = "1.9"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("simulation.Simulation1")
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
